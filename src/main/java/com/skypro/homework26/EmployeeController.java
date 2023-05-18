9eb312d967e50fd1f1fe2e4450e6a27a122370e4
Денис <garaevdenis2019@gmail.com>
2023-05-18 21:56:46 +0500

    HW is done

--- src/main/java/com/skypro/homework26/EmployeeController.java
+++ src/main/java/com/skypro/homework26/EmployeeController.java
@@ -1,78 +1,36 @@
 package com.skypro.homework26;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/employee")
+
 public class EmployeeController {
-    private static Employee[] employees = new Employee[5];
+    private final EmployeeInterface employeeInterface;
 
-    public static void main(String[] args) {
-        employees[0] = new Employee("Lapshin Alexander Nikolaevich", 1, 5000);
-        employees[1] = new Employee("Kurdyum Alexey Anatolyevich", 2, 3000);
-        employees[2] = new Employee("Gaziev Zhaudat Yaukhatovichh", 3, 4000);
-        employees[3] = new Employee("Shestakov Daniil Zakharovich", 2, 3500);
-        employees[4] = new Employee("Morozov Dmitry Yaroslavovich", 1, 4500);
-
-        printEmployees();
-        calculateTotalExpense(employees);
-        findEmployeeWithMinSalary(employees);
-        findEmployeeWithMaxSalary(employees);
-        calculateMiddleExpense(employees);
-        printEmployeesNames();
+    public EmployeeController(EmployeeInterface employeeInterface) {
+        this.employeeInterface = employeeInterface;
     }
 
-    public static void printEmployees() {
-        for (Employee employee : employees) {
-            System.out.println(employee);
+    @GetMapping("/add")
+    public Employee addEmployee(@RequestParam String firstname, @RequestParam String surname) {
+        return employeeInterface.add(firstname, surname);
+    }
+    @GetMapping("/remove")
+    public Employee removeEmployee(@RequestParam String firstname, @RequestParam String surname) {
+        return employeeInterface.remove(firstname, surname);
+    }
+    @GetMapping("/find")
+    public Employee findEmployee(@RequestParam String firstname, @RequestParam String surname) {
+        return employeeInterface.find(firstname, surname);
+    }
+        @GetMapping
+                public Collection<Employee> findAll() {
+            return employeeInterface.findAll();
         }
     }
-
-    public static void calculateTotalExpense(Employee[] employees) {
-        int sum = 0;
-        for (Employee employee : employees) {
-            sum += employee.getSalary();
-
-        }
-        System.out.println("Сумма трат на зарплаты составила =  " + sum);
-    }
-
-    public static void findEmployeeWithMinSalary(Employee[] employees) {
-        int minSalary = Integer.MAX_VALUE;
-        Employee resultedEmployee = null;
-        for (Employee employee : employees) {
-            if (employee.getSalary() < minSalary) {
-                minSalary = employee.getSalary();
-                resultedEmployee = employee;
-            }
-        }
-        System.out.println(resultedEmployee);
-    }
-
-    public static void findEmployeeWithMaxSalary(Employee[] employees) {
-        int maxSalary = Integer.MIN_VALUE;
-        Employee resultedEmployee = null;
-        for (Employee employee : employees) {
-            if (employee.getSalary() > maxSalary) {
-                maxSalary = employee.getSalary();
-                resultedEmployee = employee;
-            }
-        }
-        System.out.println(resultedEmployee);
-    }
-
-    public static void calculateMiddleExpense(Employee[] employees) {
-        int middleSum = 0;
-        int sum = 0;
-        int day = 30;
-        for (Employee employee : employees) {
-            sum += employee.getSalary();
-            middleSum = sum / day;
-
-        }
-        System.out.println("Сумма трат на зарплаты составила =  " + middleSum);
-    }
-
-    public static void printEmployeesNames() {
-        for (Employee employee : employees) {
-            System.out.println(employee.getFullName());
-        }
-    }
-}
-}
